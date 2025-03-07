8c91dcc29f06026fe8c7bddbdae5e4890283d0bf
Carlo Costa <carlo.costa92@gmail.com>
2025-03-07 14:14:39 +0100

    feat: problem solved

--- Snack 1/app.js
+++ Snack 1/app.js
@@ -4,3 +4,52 @@
 // Stampare a schermo la bici con peso minore.
 
 console.log("carlo")
+
+//array biciclette
+const biciclette = [
+
+    {
+        "nome" : "bianchi",
+        "peso" : 10
+    },
+
+    {
+        "nome" : "rossi",
+        "peso" : 8
+
+    },
+
+    {
+        "nome" : "verdi",
+        "peso" : 12
+
+    }
+
+]
+
+console.log(biciclette);
+
+// pusho dentro un nuovo array tutti i pesi
+const pesi = [];
+for (let i = 0; i < biciclette.length; i++) {
+  pesi.push(biciclette[i].peso);
+}
+
+console.log(pesi);
+
+
+// ciclo for per trovare quello che pesa di meno
+let pesoMinimo = pesi[0]; 
+
+for (let i = 0; i < pesi.length; i++) { 
+  if (pesi[i] < pesoMinimo) {
+    pesoMinimo = pesi[i]; 
+  }
+}
+
+console.log(pesoMinimo); 
+
+// stampo su html
+
+
+
